536ae2cc1770b52ef4080eac1d0eb11d286694f9
atj <adambsimpson@gmail.com>
2017-03-20 14:04:59 -0400

    Rename working copy and fix type

--- Simulation/Source/main.cpp
+++ Simulation/Source/main.cpp
@@ -27,40 +27,52 @@ THE SOFTWARE.
 #include "parameters.h"
 #include "particles.h"
 #include "distributor.h"
-#include "adios_writer.h"
 
 int main(int argc, char *argv[]) {
   try {
     // Probably should have some kind of builder
     sim::Distributor <float,three_dimensional> distributor;
 
-    auto parameters  = new sim::Parameters<float, three_dimensional>{"params.ini"};
+    auto parameters  = new sim::Parameters<float, three_dimensional>;
 
+    distributor.sync_from_renderer(*parameters);
+
+    // @todo enforce dynamic construction requirement
     // The use of new isn't enforced but is required as managed memory is used
     auto particles = new sim::Particles<float, three_dimensional>(*parameters);
-
-    sim::AdiosWriter<float, three_dimensional> writer{"adios_writer.xml",distributor};
 
     // After particles have been created construct initial fluid
     distributor.initialize_fluid(*particles, *parameters);
 
-    int64_t step = 0;
+    // Sync initial particle configuration
+    distributor.sync_to_renderer(*particles);
 
-    // Number of simulation steps per render step
+    // Set algorithms
+//    sim::algorithms::execution_policy_ =
+
+    int64_t frame = 0;
+
+    // Number of simulation frames per render frame
     const int target_fps = 60;
-    const int steps_per_save = (int)std::round(1.0 / parameters->time_step() / target_fps);
+    const int frames_per_update = (int)std::round(1.0 / parameters->time_step() / target_fps);
+    std::cout<<"Compute updating renderer every "<<frames_per_update<<" frames"<<std::endl;
 
     // Main time step loop
-    const int step_count = 300;
-    for(int i=0; i<step_count; i++) {
+    while(parameters->simulation_active()) {
+      if(frame % frames_per_update == 0)
+        distributor.sync_from_renderer(*parameters);
+
       if(parameters->compute_active()) {
         distributor.process_parameters(*parameters, *particles);
+
+          // Only for sim_algorithms_on_the_fly
+//        sim::algorithms::process_parameters(*parameters);
 
         particles->apply_external_forces(distributor.resident_span());
 
         particles->predict_positions(distributor.resident_span());
 
-        distributor.balance_domains();
+//        distributor.balance_domains();
 
         distributor.domain_sync(*particles);
 
@@ -72,34 +84,38 @@ int main(int argc, char *argv[]) {
           particles->compute_densities(distributor.resident_span());
 
           particles->compute_pressure_lambdas(distributor.resident_span());
-          distributor.initiate_sync_halo_scalar(particles->lambdas());
-          distributor.finalize_sync_halo_scalar();
+//          distributor.initiate_sync_halo_scalar(particles->lambdas());
+//          distributor.finalize_sync_halo_scalar();
 
           particles->compute_pressure_dps(distributor.resident_span(), sub);
 
           particles->update_position_stars(distributor.resident_span());
-          distributor.initiate_sync_halo_vec(particles->position_stars());
-          distributor.finalize_sync_halo_vec();
+//          distributor.initiate_sync_halo_vec(particles->position_stars());
+//          distributor.finalize_sync_halo_vec();
 
-          particles->apply_surface_tension(distributor.local_span(), distributor.resident_span());
+          //        particles_.compute_surface_lambdas(distributor_.local_span());
+          //        particles_.compute_surface_dps(distributor_.local_span(), sub);
+
+//        particles->apply_surface_tension(distributor.local_span(), distributor.resident_span());
+
         }
 
         particles->update_velocities(distributor.local_span());
 
-        distributor.initiate_sync_halo_scalar(particles->densities());
-        distributor.finalize_sync_halo_scalar();
+//        distributor.initiate_sync_halo_scalar(particles->densities());
+//        distributor.finalize_sync_halo_scalar();
 
         particles->apply_surface_tension(distributor.local_span(), distributor.resident_span());
 
         particles->apply_viscosity(distributor.resident_span());
 
-        distributor.initiate_sync_halo_vec(particles->velocities());
-        distributor.finalize_sync_halo_vec();
+//        distributor.initiate_sync_halo_vec(particles->velocities());
+//        distributor.finalize_sync_halo_vec();
 
         particles->compute_vorticity(distributor.resident_span());
 
-        distributor.initiate_sync_halo_vec(particles->scratch());
-        distributor.finalize_sync_halo_vec();
+//        distributor.initiate_sync_halo_vec(particles->scratch());
+//        distributor.finalize_sync_halo_vec();
 
         particles->apply_vorticity(distributor.resident_span());
 
@@ -107,13 +123,11 @@ int main(int argc, char *argv[]) {
 
         particles->update_positions(distributor.resident_span());
 
-        // Needs to be done once per rendered step
-        if(step % steps_per_save == 0) {
-          std::cout<<"Writing step "<< step << std::endl;
-          writer.write_particles(*particles);
-        }
+        // Needs to be done once per rendered frame
+        if(frame % frames_per_update == 0)
+          distributor.sync_to_renderer(*particles);
 
-        step++;
+        frame++;
 
         distributor.invalidate_halo(*particles);
       }
